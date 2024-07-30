050ef0ed7f4b4a7bdba34f81335ce87dbc52de3d
LazicNikola <lazic1213@gmail.com>
2024-07-31 01:15:06 +0200

    JS Header, JS Nav Bar, JS Scroll-Up Button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,50 @@
-/*=============== SHOW MENU ===============*/
+// <============================= HEADER =============================>
 
-/*=============== REMOVE MENU MOBILE ===============*/
+// <============== TOP HEADER ==============>
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+const documentElement = document.documentElement;
+const showTopHeaderBackground = () => {
+  const headerElement = document.querySelector("header");
+  if (documentElement.scrollTop > 0) {
+    headerElement.classList.add("show-top-header");
+  } else {
+    headerElement.classList.remove("show-top-header");
+  }
+};
+window.addEventListener("scroll", showTopHeaderBackground);
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+// <=========== RIGHT NAV BAR OPEN ===========>
 
-/*=============== SHOW SCROLL UP ===============*/
+const navBarOpenButton = document.querySelector(".nav-menu-open-button");
+const rightNavBar = document.querySelector(".nav-menu");
+const showRightNavBar = () => {
+  if (rightNavBar) {
+    rightNavBar.classList.add("show-right-nav-bar");
+  }
+};
+navBarOpenButton.addEventListener("click", showRightNavBar);
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+// <=========== RIGHT NAV BAR EXIT ===========>
 
-/*=============== CALCULATE JS ===============*/
+const navBarExitButton = document.querySelector(".nav-menu-exit-button");
+const exitRightNavBar = () => {
+  rightNavBar.classList.remove("show-right-nav-bar");
+};
+navBarExitButton.addEventListener("click", exitRightNavBar);
 
-/*=============== EMAIL JS ===============*/
+// <============================= SCROLL-UP BUTTON =============================>
+
+const scrollUpButton = document.querySelector(".scroll-up");
+const showScrollUpButton = () => {
+  if (scrollUpButton && documentElement.scrollTop > 200) {
+    scrollUpButton.classList.add("show-scroll-up-button");
+  } else {
+    scrollUpButton.classList.remove("show-scroll-up-button");
+  }
+};
+window.addEventListener("scroll", showScrollUpButton);
+
+const scrollToTop = () => {
+  documentElement.scrollTo({ top: 0, behavior: "smooth" });
+};
+scrollUpButton.addEventListener("click", scrollToTop);
