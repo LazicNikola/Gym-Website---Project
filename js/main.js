35e5e407893098d9601b5a62482fe68b7bf01962
LazicNikola <lazic1213@gmail.com>
2024-08-02 01:49:39 +0200

    JS Nav Bar Links, JS Reveal Sections

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 // <============================= HEADER =============================>
-
-// <============== TOP HEADER ==============>
-
+// <================ LOGO SCROLL-TOP ================>
 const documentElement = document.documentElement;
+const logoTextHeader = document.querySelector(".gym-name");
+const logoImageHeader = document.querySelector(".gym-name-img");
+const logoScrollingToTop = () => {
+  documentElement.scrollTo({ top: 0 });
+  setTimeout(() => {
+    window.location.reload();
+  }, 50);
+};
+logoTextHeader.addEventListener("click", logoScrollingToTop);
+logoImageHeader.addEventListener("click", logoScrollingToTop);
+
+// <============== TOP HEADER BACKGROUND ==============>
 const showTopHeaderBackground = () => {
   const headerElement = document.querySelector("header");
   if (documentElement.scrollTop > 0) {
@@ -14,7 +24,6 @@ const showTopHeaderBackground = () => {
 window.addEventListener("scroll", showTopHeaderBackground);
 
 // <=========== RIGHT NAV BAR OPEN ===========>
-
 const navBarOpenButton = document.querySelector(".nav-menu-open-button");
 const rightNavBar = document.querySelector(".nav-menu");
 const showRightNavBar = () => {
@@ -25,13 +34,45 @@ const showRightNavBar = () => {
 navBarOpenButton.addEventListener("click", showRightNavBar);
 
 // <=========== RIGHT NAV BAR EXIT ===========>
-
 const navBarExitButton = document.querySelector(".nav-menu-exit-button");
+const navLinks = document.querySelectorAll(".nav-link");
 const exitRightNavBar = () => {
   rightNavBar.classList.remove("show-right-nav-bar");
 };
 navBarExitButton.addEventListener("click", exitRightNavBar);
+navLinks.forEach((e) => e.addEventListener("click", exitRightNavBar));
 
+// <=============================  NAV BAR SCROLL-TO =============================>
+const homeLink = document.querySelector(".nav-home");
+
+const scrollToHomePage = (e) => {
+  e.preventDefault();
+  documentElement.scrollTo({ top: 0, behavior: "smooth" });
+};
+homeLink.addEventListener("click", scrollToHomePage);
+
+const programLink = document.querySelector(".nav-program");
+const programSection = document.querySelector(".program");
+
+const chooseUsLink = document.querySelector(".nav-choose-us");
+const chooseUsSection = document.querySelector(".choose-us");
+
+const scrollToSectionOfPage = (e, section) => {
+  e.preventDefault();
+  section.scrollIntoView({ behavior: "smooth" });
+};
+
+programLink.addEventListener("click", (e) =>
+  scrollToSectionOfPage(e, programSection)
+);
+
+chooseUsLink.addEventListener("click", (e) =>
+  scrollToSectionOfPage(e, chooseUsSection)
+);
+
+//
+//
+//
 // <============================= CALCULATE BMI =============================>
 const calculateBmiButton = document.querySelector(".calculate-button");
 const calculateBmi = (e) => {
@@ -83,8 +124,10 @@ const calculateBmi = (e) => {
 };
 calculateBmiButton.addEventListener("click", calculateBmi);
 
+//
+//
+//
 // <============================= EMAIL INPUT =============================>
-
 const subscribeButton = document.querySelector(".subscribe-button");
 const subscribing = (e) => {
   e.preventDefault();
@@ -95,11 +138,15 @@ const subscribing = (e) => {
   if (regexEmail.test(emailInput.value)) {
     subscribeMessage.classList.add("correct-email");
     subscribeMessage.classList.remove("wrong-email");
-    subscribeMessage.textContent = "You subscribed successfully!";
+    subscribeMessage.textContent = "You have successfully subscribed!";
+  } else if (emailInput.value === "") {
+    subscribeMessage.classList.add("wrong-email");
+    subscribeMessage.classList.remove("correct-email");
+    subscribeMessage.textContent = "You need to enter your email address";
   } else {
     subscribeMessage.classList.add("wrong-email");
     subscribeMessage.classList.remove("correct-email");
-    subscribeMessage.textContent = "Your email address is incorrect.";
+    subscribeMessage.textContent = "Your email address is incorrect";
   }
   setTimeout(() => {
     subscribeMessage.textContent = "";
@@ -109,7 +156,6 @@ const subscribing = (e) => {
 subscribeButton.addEventListener("click", subscribing);
 
 // <============================= SCROLL-UP BUTTON =============================>
-
 const scrollUpButton = document.querySelector(".scroll-up");
 const showScrollUpButton = () => {
   if (scrollUpButton && documentElement.scrollTop > 200) {
@@ -124,3 +170,29 @@ const scrollToTop = () => {
   documentElement.scrollTo({ top: 0, behavior: "smooth" });
 };
 scrollUpButton.addEventListener("click", scrollToTop);
+
+//
+//
+//
+// <============================= REVEAL SECTIONS =============================>
+// <============== HOME SECTION REVEAL ==============>
+const homeLeft = document.querySelector(".home-left");
+const homeRight = document.querySelector(".home-right");
+
+const revealHomeLeft = () => {
+  homeLeft.classList.add("reveal-home-left");
+};
+window.addEventListener("load", revealHomeLeft);
+
+const revealHomeRight = () => {
+  homeRight.classList.add("reveal-home-right");
+};
+window.addEventListener("load", revealHomeRight);
+
+// <============== PARTNER SECTION REVEAL ==============>
+const partners = document.querySelector(".partner-logo");
+
+const revealPartnerSection = () => {
+  partners.classList.add("reveal-partners");
+};
+window.addEventListener("scroll", revealPartnerSection);
